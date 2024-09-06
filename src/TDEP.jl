5c4c22bc6e5982fdb935a169af1518e912a401ab
Lorenzo Monacelli <mesonepigreco@gmail.com>
2024-09-06 22:39:15 +0200

    TDEP works

--- src/TDEP.jl
+++ src/TDEP.jl
@@ -1,5 +1,15 @@
 module TDEP
 
-greet() = print("Hello World!")
+using LinearAlgebra
+using ForwardDiff
+using ReverseDiff
+using Optim
+using FileIO
+using JLD2
+using AtomicEnsemble
+
+include("tdep_core.jl")
+
+export tdep_fit!
 
 end # module TDEP
