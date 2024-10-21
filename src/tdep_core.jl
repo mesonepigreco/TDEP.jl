aae22d0ae9c4da57ee4cf90e0da9bf2408690a21
Lorenzo Monacelli <mesonepigreco@gmail.com>
2024-10-21 16:33:58 +0200

    Now it should be able to run with the symmetries

--- src/tdep_core.jl
+++ src/tdep_core.jl
@@ -31,7 +31,8 @@ Perform the TDEP analysis on the ensemble data.
 This routine exploits the analytical solution to the TDEP equations: 
 the displacement-displacement correlation matrix.
 """
-function tdep_anal!(fc_matrix :: Matrix{T}, centroids :: Vector, ensemble :: StandardEnsemble, kT; apply_asr = true) where T
+function tdep_anal!(fc_matrix :: Matrix{T}, centroids :: Vector, ensemble :: StandardEnsemble, kT; apply_asr = true, 
+    symmetry_group = nothing) where T
     n_configs = length(ensemble)
     nat = length(ensemble.structures[1])
 
@@ -51,6 +52,11 @@ function tdep_anal!(fc_matrix :: Matrix{T}, centroids :: Vector, ensemble :: Sta
         end
     end
     centroids ./= n_configs
+
+    # Apply the symmetries on the centroids
+    if symmetry_group != nothing
+        symmetry_group.symmetrize_centroids!(centroids)
+    end
 
     for i in 1:n_configs
         u_disp[:, i] .-= centroids
@@ -82,6 +88,11 @@ function tdep_anal!(fc_matrix :: Matrix{T}, centroids :: Vector, ensemble :: Sta
             #@views mul!(fc_matrix, p[:, μ], p[:, μ]', 1.0 / (ω[μ]), 1.0)
         end
     end
+
+    # Impose the symmetries
+    if symmetry_group != nothing
+        symmetry_group.symmetrize_fc!(fc_matrix)
+    end
 end
 
 @doc raw"""
@@ -95,6 +106,7 @@ The final force constants and centroids are stored in the `fc_matrix` and `centr
 
 """
 function tdep_fit!(fc_matrix :: AbstractMatrix, centroids :: AbstractVector, ensemble :: StandardEnsemble;
+        symmetry_group = nothing,
         optimizer = LBFGS(), optimizer_options = Optim.options(iterations=1000, show_trace = true))
     # Check consistency between sizes
     n_structures = length(ensemble)
@@ -109,11 +121,32 @@ function tdep_fit!(fc_matrix :: AbstractMatrix, centroids :: AbstractVector, ens
 
     fitted_forces = zeros(T, nat3)
     u_disps = zeros(T, nat3)
-    params = zeros(T, count_params(nat))
 
+    # If the symmetries are provided, extract the generators
+    n_params = count_params(nat)
+    vector_generators = nothing
+    fc_generators = nothing
+    if symmetry_group != nothing
+        # Generators of the displacements
+        vector_generators = AtomicSymmetries.get_vector_generators(symmetry_group)
+        fc_generators = AtomicSymmetries.get_matrix_generators(symmetry_group)
+
+        # Count the number of parameters
+        n_params = length(vector_generators) + length(fc_generators)
+    end
+    params = zeros(T, n_params)
+
+    @info "Fitting the force constants with $n_params parameters"
+
+    # Get the ASR functions
+    asr_fc! = ASRMatrixConstraint!(3)
+    asr_centroids! = ASRVectorConstraint!(3)
 
     # Fill the initial parameters with the provided matrix
-    mat2param_vect!(params, fc_matrix, centroids)
+    mat2param_vect!(params, fc_matrix, centroids; 
+                    vector_generators = vector_generators, 
+                    fc_generators = fc_generators, 
+                    symmetry_group = symmetry_group)
 
     function least_squares(params :: AbstractArray{T}) :: T where T
         # Unpack the parameters (careful with ForwardDiff)
@@ -123,7 +156,14 @@ function tdep_fit!(fc_matrix :: AbstractMatrix, centroids :: AbstractVector, ens
         u_disps = my_cache.u_disps
         fitted_forces = my_cache.fitted_forces
 
-        param_vect2mat!(fc_matrix, centroids, params)
+        param_vect2mat!(fc_matrix, centroids, params; 
+                        vector_generators = vector_generators, 
+                        fc_generators = fc_generators, 
+                        symmetry_group = symmetry_group)
+
+        # Apply the ASR!
+        asr_fc!(fc_matrix)
+        asr_centroids!(centroids)
 
         # Get the displacements
         least_squares_res = zero(T)
@@ -135,7 +175,7 @@ function tdep_fit!(fc_matrix :: AbstractMatrix, centroids :: AbstractVector, ens
                 end
             end
             #fitted_forces .= fc_matrix * u_disps
-            mul!(fitted_forces, fc_matrix, u_disps)
+            mul!(fitted_forces, fc_matrix, u_disps, -1.0, 0.0)
 
             for j in 1:nat
                 for k in 1:3
@@ -164,20 +204,42 @@ end
 Unpack the parameters vector into the force constants matrix and centroids vector.
 Inplace modifies fc_matrix and centorids.
 """
-function param_vect2mat!(fc_matrix :: AbstractArray{T}, centroids :: AbstractArray{T}, params :: AbstractArray{T}) where T
-    # Unpack the parameters
-    count = 1
-    nat3 = size(fc_matrix, 1)
-    for i in 1:nat3
-        for j in i:nat3
-            fc_matrix[i, j] = params[count]
-            fc_matrix[j, i] = params[count]
+function param_vect2mat!(fc_matrix :: AbstractArray{T}, centroids :: AbstractArray{T}, params :: AbstractArray{T}; 
+        vector_generators = nothing,
+        fc_generators = nothing,
+        symmetry_group = nothing
+    ) where T
+    if symmetry_group === nothing
+        # Unpack the parameters
+        count = 1
+        nat3 = size(fc_matrix, 1)
+        for i in 1:nat3
+            for j in i:nat3
+                fc_matrix[i, j] = params[count]
+                fc_matrix[j, i] = params[count]
+                count += 1
+            end
+        end
+        for i in 1:nat3
+            centroids[i] = params[count]
             count += 1
         end
-    end
-    for i in 1:nat3
-        centroids[i] = params[count]
-        count += 1
+    else
+        n_vectors = length(vector_generators)
+        n_matrices = length(fc_generators)
+
+        if n_vectors > 0
+            AtomicSymmetries.get_vector_from_generators!(centroids,
+                                                         vector_generators, 
+                                                         params[1:n_vectors], 
+                                                         symmetry_group)
+        else
+            centroids .= 0
+        end
+        AtomicSymmetries.get_fc_from_generators!(fc_matrix, 
+                                                 fc_generators, 
+                                                 params[n_vectors+1:end], 
+                                                 symmetry_group)
     end
 end
 
@@ -187,18 +249,37 @@ end
 Pack the force constants matrix and centroids vector into a parameters vector.
 Inplace modifies params.
 """
-function mat2param_vect!(params :: AbstractVector, fc_matrix :: AbstractMatrix, centroids :: AbstractVector) 
-    count = 1
-    nat3 = size(fc_matrix, 1)
-    for i in 1:nat3
-        for j in i:nat3
-            params[count] = fc_matrix[i, j]
+function mat2param_vect!(params :: AbstractVector, fc_matrix :: AbstractMatrix, centroids :: AbstractVector; 
+        vector_generators = nothing,
+        fc_generators = nothing,
+        symmetry_group = nothing) 
+    if symmetry_group === nothing
+        count = 1
+        nat3 = size(fc_matrix, 1)
+        for i in 1:nat3
+            for j in i:nat3
+                params[count] = fc_matrix[i, j]
+                count += 1
+            end
+        end
+        for i in 1:nat3
+            params[count] = centroids[i]
             count += 1
         end
-    end
-    for i in 1:nat3
-        params[count] = centroids[i]
-        count += 1
+    else
+        n_vectors = length(vector_generators)
+        n_matrices = length(fc_generators)
+
+        if n_vectors > 0
+            @views AtomicSymmetries.get_coefficients_from_fc!(params[1:n_vectors],
+                                                              centroids, 
+                                                              vector_generators, 
+                                                              symmetry_group)
+        end
+        @views AtomicSymmetries.get_coefficients_from_fc!(params[n_vectors+1:end],
+                                                          fc_matrix, 
+                                                          fc_generators, 
+                                                          symmetry_group)
     end
 end
 
